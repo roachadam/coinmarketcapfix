d44335fcd466187dd3a5c1de68e9ddf95ce0ba48
Adam Roach <adamroach504@gmail.com>
2018-01-03 23:04:27 -0600

    first commit

---
+++ coinmarketcapfix.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         CoinMarketCapFix
+// @description  Extends the coin graph to to a larger size.
+// @author       Adam Roach - github.com/roachadam
+// @version      1.0
+// @grant        none
+// @match 		 https://coinmarketcap.com/*
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Grabs the table using jQuery and sets the table to the larger style class.
+    $(".col-lg-10").removeClass("col-lg-10").addClass("col-lg-12");
+})();
+
+
+
+
